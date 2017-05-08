1443bb3216b40948dee024692779acd764fcb292
ckszabi <ckszabi@gmail.com>
2017-05-08 11:04:21 +0300

    Update and rename deploy.sh to before_deploy.sh

--- before_deploy.sh
+++ before_deploy.sh
@@ -2,8 +2,10 @@
 set -ev
 
 echo 'getting ready to deploy'
+pwd
+
 ELASTIC_BEANSTALK_LABEL="commit: $(git rev-parse HEAD)"
 echo $ELASTIC_BEANSTALK_LABEL
 echo $ELASTIC_BEANSTALK_DESCRIPTION
-pwd
+
 zip -r deploy_package.zip . -x ".git/*" -x "*/.git/*" -x "vendor/*"
