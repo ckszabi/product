df7676b71c6f561cbf0934f507de6e2818a9fbb0
ckszabi <ckszabi@gmail.com>
2017-05-09 09:07:39 +0300

    Update before_deploy.sh

--- before_deploy.sh
+++ before_deploy.sh
@@ -4,7 +4,7 @@ echo 'getting ready to deploy'
 PACKAGE_VERSION=$(node -p -e "require('./package.json').version")
 echo "Package version: $PACKAGE_VERSION"
 
-export ELASTIC_BEANSTALK_LABEL="$PACKAGE_VERSION_commit_$(git rev-parse HEAD)_job_$TRAVIS_JOB_ID";
+export ELASTIC_BEANSTALK_LABEL="${PACKAGE_VERSION}_commit_$(git rev-parse HEAD)_job_$TRAVIS_JOB_ID";
 echo "Using label: $ELASTIC_BEANSTALK_LABEL"
 
 #export ELASTIC_BEANSTALK_DESCRIPTION="abc";
