5d025c8903a85f305324152042dd9e92ce3f00c9
ckszabi <ckszabi@gmail.com>
2017-05-09 09:23:32 +0300

    Update before_deploy.sh

--- before_deploy.sh
+++ before_deploy.sh
@@ -4,9 +4,9 @@ echo 'getting ready to deploy'
 PACKAGE_VERSION=$(node -p -e "require('./package.json').version")
 echo "Package version: $PACKAGE_VERSION"
 
-TIMESTAMP="$(date +"%T")"
+TIMESTAMP="$(date +"%T") UTC"
 
-export ELASTIC_BEANSTALK_LABEL="v${PACKAGE_VERSION} build_${TRAVIS_BUILD_NUMBER} at_${TIMESTAMP}";
+export ELASTIC_BEANSTALK_LABEL="v${PACKAGE_VERSION} (build_${TRAVIS_BUILD_NUMBER} at_${TIMESTAMP})";
 echo "Using label: $ELASTIC_BEANSTALK_LABEL"
 
 #export ELASTIC_BEANSTALK_DESCRIPTION="abc";
