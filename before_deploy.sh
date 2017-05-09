1832a2de3eb4a74e290f60c1f5396150a47a0277
ckszabi <ckszabi@gmail.com>
2017-05-09 09:17:29 +0300

    version. build and timestamp as label

--- before_deploy.sh
+++ before_deploy.sh
@@ -4,7 +4,9 @@ echo 'getting ready to deploy'
 PACKAGE_VERSION=$(node -p -e "require('./package.json').version")
 echo "Package version: $PACKAGE_VERSION"
 
-export ELASTIC_BEANSTALK_LABEL="${PACKAGE_VERSION}_commit_$(git rev-parse HEAD)_job_$TRAVIS_JOB_ID";
+TIMESTAMP="$(date +"%T")"
+
+export ELASTIC_BEANSTALK_LABEL="v${PACKAGE_VERSION} build_${TRAVIS_BUILD_NUMBER} at_${TIMESTAMP}";
 echo "Using label: $ELASTIC_BEANSTALK_LABEL"
 
 #export ELASTIC_BEANSTALK_DESCRIPTION="abc";
