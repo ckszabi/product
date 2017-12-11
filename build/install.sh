1f5eed619c26e6ddb2fe9965447b38aeba9f4eb8
ckszabi <ckszabi@gmail.com>
2017-12-11 15:07:01 +0200

    travis build install script added

---
+++ build/install.sh
@@ -0,0 +1,17 @@
+composer install
+
+
+  # set up python and it's package manager
+sudo apt-get update 
+sudo apt-get install -y python3 python3-pip
+pip3 install -U pytest
+  # install nodejs and it's package manager
+  #apt-get install -y gnupg
+  #curl -sL https://deb.nodesource.com/setup_6.x | bash -
+  #apt-get install -y nodejs
+  #
+  # install apache and it's modules
+  # apt-get install -y apache2
+  # apache2ctl configtest
+  ##apt-get install -y libapache2-mod-php5
+  #/etc/init.d/apache2 restart
