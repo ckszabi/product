84c17e6021fde6b545ae88ae8e5fe10063d3d5d2
ckszabi <ckszabi@gmail.com>
2017-12-12 14:31:05 +0200

    [skip ci] before install comments

--- build/before_install.sh
+++ build/before_install.sh
@@ -3,17 +3,16 @@
 #In a before_install step, you can install additional dependencies required by your project such as Ubuntu packages or custom services.
 
 # set up python and it's package manager
-sudo apt-get update 
-sudo apt-get install -y python3 
-#python3-pip
+#sudo apt-get update 
+#sudo apt-get install -y python3  python3-pip
 
-python -V
-python3 -V
-pip3.5 -V
-pip -V
-pytest -V
+#python -V
+#python3 -V
+#pip3.5 -V
+#pip -V
+#pytest -V
 
-pip3.5 install -U pytest
+#pip3.5 install -U pytest
   # install nodejs and it's package manager
   #apt-get install -y gnupg
   #curl -sL https://deb.nodesource.com/setup_6.x | bash -
