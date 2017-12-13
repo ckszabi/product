4e038959f2c3bedcc1d70b6c3ee4e4847ebcb05b
ckszabi <ckszabi@gmail.com>
2017-12-13 16:40:09 +0200

    chromedriver specified in test

--- test_selenium.py
+++ test_selenium.py
@@ -1,9 +1,13 @@
+import os
 from selenium import webdriver
 
 class TestTest():
 
     def test_test(self):
-        driver = webdriver.Chrome()
+        chromedriver = "~/chromedriver"
+        os.environ["webdriver.chrome.driver"] = chromedriver
+        driver = webdriver.Chrome(chromedriver)
+        
         driver.implicitly_wait(3)
         driver.get("http://staging.togital.no/konkurrenten/")
         driver.save_screenshot("screenshot.png")
