bbfeb390c4a4c925ae03391d90e0116902f6026b
ckszabi <ckszabi@gmail.com>
2017-12-13 11:50:32 +0200

    Update script.sh

--- build/script.sh
+++ build/script.sh
@@ -1,13 +1,28 @@
 #!/bin/bash
 
 # getting versions
+echo "python3 version:"
 python3 -V
+
+echo "pip3 version:"
 pip3 -V
+
+echo "pytest version:"
 pytest --version
+
+echo "starting pytest..."
 pytest
+
 # check node and start node test
+echo "node version:"
 node -v
+
+echo "npm version:"
 npm -v
+
+echo "installing node dependencies"
 npm install
+
+echo "starting node selenium tests..."
 EID=${TRAVIS_JOB_NUMBER} node selenium.js
 node selenium.js
