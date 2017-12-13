1dc296bd902f2b60a86b75346880562fad8ee86b
ckszabi <ckszabi@gmail.com>
2017-12-13 16:25:17 +0200

    Update before_install.sh

--- build/before_install.sh
+++ build/before_install.sh
@@ -24,7 +24,7 @@ sudo ln -s ~/chromedriver /usr/local/bin/chromedriver
 
 echo "whereis chromedriver:"
 whereis chromedriver
-chromedriver
+~/chromedriver
 
 # set up python and it's package manager
 #sudo apt-get update 
