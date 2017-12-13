4dae0654163a35884159590ea7e16f0afe82165d
ckszabi <ckszabi@gmail.com>
2017-12-13 17:13:04 +0200

    Update test_selenium.py

--- test_selenium.py
+++ test_selenium.py
@@ -9,7 +9,7 @@ class TestTest():
         
         options = webdriver.ChromeOptions()
         options.binary_location = '/usr/bin/google-chrome-stable'
-        options.set_headless(true);
+        options.set_headless(1);
         options.add_argument("--disable-gpu");
         #service_log_path = "{}/chromedriver.log".format(outputdir)
         #service_args = ['--verbose']
