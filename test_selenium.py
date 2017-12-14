86d9f7e3440f502c778f2cc5b281e77b340773f0
ckszabi <ckszabi@gmail.com>
2017-12-14 16:11:55 +0200

    no more explicit chromedriver path

--- test_selenium.py
+++ test_selenium.py
@@ -4,17 +4,18 @@ from selenium import webdriver
 class TestTest():
 
     def test_test(self):
-        chromedriver = "/home/travis/chromedriver"
-        os.environ["webdriver.chrome.driver"] = chromedriver
+        #chromedriver = "/home/travis/chromedriver"
+        #os.environ["webdriver.chrome.driver"] = chromedriver
         
-        options = webdriver.ChromeOptions()
-        options.binary_location = '/usr/bin/google-chrome-stable'
-        options.set_headless(1);
-        options.add_argument("--disable-gpu");
+        #options = webdriver.ChromeOptions()
+        #options.binary_location = '/usr/bin/google-chrome-stable'
+        #options.set_headless(1);
+        #options.add_argument("--disable-gpu");
         #service_log_path = "{}/chromedriver.log".format(outputdir)
         #service_args = ['--verbose']
         
-        driver = webdriver.Chrome(chromedriver, chrome_options=options)
+        #driver = webdriver.Chrome(chromedriver, chrome_options=options)
+        driver = webdriver.Chrome()
         
         driver.implicitly_wait(3)
         driver.get("http://staging.togital.no/konkurrenten/")
